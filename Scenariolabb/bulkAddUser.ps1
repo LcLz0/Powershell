55a76c6839863f9f402ada7d97c3e7550edc3a82
Lz0 <netharion@gmail.com>
2020-12-03 21:13:59 +0100

    Init SAM and UPN

--- Scenariolabb/bulkAddUser.ps1
+++ Scenariolabb/bulkAddUser.ps1
@@ -3,40 +3,40 @@ powershell.exe -File "F:\grpCreate.ps1"
 
 # Loop through user list and create entries
 $Users = Import-Csv -Path "F:\anvandarlistatest.csv"
+
 foreach ($User in $Users)
-{ # Main block
+{   # Main block
 
 	# Let's populate some variables, SAM creation further down
     $FirstName = $User.fName
     $LastName = $User.lName
     $Displayname = $User.dName
-  #  $UPN = $User.upn
     $Description = $User.avdelning
     $Mobile = $User.tele
     $Address = $User.adress
     $PostalCode = $User.postNr
     $City = $User.stad
-    # Remember to change "domnain" to actual domain name
+    # Remember to change "domain" to actual domain name
     $OU = "OU=Användare,OU=$City,DC=domain,DC=nu"
 
 	# SAM name creation https://4sysops.com/archives/strings-in-powershell-replace-compare-concatenate-split-substring/
 	# $name.Remove(0.99) Remove everything except first char
 	# STRING.ToLower() to change all to lowercase
-	# $SAM = "$FirstName $LastName"
+	 $SAM = $FirstName.Remove(0.50) + $LastName
+   $UPN = "$SAM@jultomten.nu"
 
 	# Run commands with above vars
     New-ADUser -Name "$Displayname" `
     -GivenName $FirstName `
     -Surname $LastName `
-  #  -SamAccountName $SAM `
-  #  -UserPrincipalName $UPN `
     -Description $Description `
     -MobilePhone $Mobile `
     -StreetAddress "$Address" `
     -PostalCode "$PostalCode" `
     -City $City `
-    -Path "$OU"
-
+    -Path "$OU" `
+    -SamAccountName $SAM `
+    -UserPrincipalName $UPN 
 
     # Add users to groups, using Description to choose group
 
