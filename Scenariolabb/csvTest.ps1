e55d931205efd92349341f4855d5c1facb02f276
Lz0 <netharion@gmail.com>
2020-12-03 22:26:23 +0100

    Update

--- Scenariolabb/csvTest.ps1
+++ Scenariolabb/csvTest.ps1
@@ -1,12 +1,13 @@
-$Orter = Import-Csv -Path "C:\Users\loco\Documents\Git\Powershell\Scenariolabb\orter.csv"
+$infoDump = Import-Csv -Path "C:\Users\loco\Documents\Git\Powershell\Scenariolabb\anvandarlistatest.csv"
 $subOus = Import-Csv -Path "C:\Users\loco\Documents\Git\Powershell\Scenariolabb\subOu.csv"
 
-ForEach ($Ort in $Orter)
+ForEach ($Test in $infoDump)
 {
     # Create toplevel OU
+    echo $Test
 
-    ForEach ($subOu in $subOus)
+  <#  ForEach ($subOu in $subOus)
     {
-      # Create subOU in each toplevel OU
-    }
+      echo $subOu
+    }#>
 }
