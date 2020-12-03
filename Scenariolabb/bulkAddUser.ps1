b8812911aaf232f0af6f93222b885d75562e4c60
Lz0 <netharion@gmail.com>
2020-12-03 16:25:44 +0100

    Cleanup

--- Scenariolabb/bulkAddUser.ps1
+++ Scenariolabb/bulkAddUser.ps1
@@ -11,13 +11,23 @@ foreach ($User in $Users)
     $Address = $User.adress
     $PostalCode = $User.zip
     $City = $User.st
-    $OU = 'OU=Anvandare,OU=' + $City + ',DC=jultomten,DC=nu'
-	
+    $OU = 'OU=Användare,OU=' + $City + ',DC=domain,DC=nu'
+
 	# SAM name creation https://4sysops.com/archives/strings-in-powershell-replace-compare-concatenate-split-substring/
 	# $name.Remove(0.99) Remove everything except first char
 	# STRING.ToLower() to change all to lowercase
 	# $SAM = "$FirstName $LastName"
-	
+
 	# Run commands with above vars
-    New-ADUser -Name "$Displayname" -GivenName $FirstName -Surname $LastName -SamAccountName $SAM -UserPrincipalName $UPN -Office $Office -MobilePhone $Mobile -StreetAddress "$Address" -PostalCode "$PostalCode" -City $City -Path "$OU"
+    New-ADUser -Name "$Displayname" `
+    -GivenName $FirstName `
+    -Surname $LastName `
+    -SamAccountName $SAM `
+    -UserPrincipalName $UPN `
+    -Office $Office `
+    -MobilePhone $Mobile `
+    -StreetAddress "$Address" `
+    -PostalCode "$PostalCode" `
+    -City $City `
+    -Path "$OU"
 }
