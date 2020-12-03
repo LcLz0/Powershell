c5df87244a7a91d0eeae11dc6cf1ac7d9d040046
Lz0 <netharion@gmail.com>
2020-12-03 20:44:51 +0100

    Added locationspecific prefix to grpcreation

--- Scenariolabb/grpCreate.ps1
+++ Scenariolabb/grpCreate.ps1
@@ -2,6 +2,8 @@ $Groups = Import-Csv -Path "F:\grupper.csv"
 foreach ($Group in $Groups)
 {
   $Grp = $Group.gName
-  New-ADGroup -GroupCategory:"Security" -GroupScope:"Global" -Name:"$Grp" -Path:"OU=Grupper,OU=Stockholm,DC=jultomten,DC=nu" -SamAccountName:"$grp" -Server:"DC.jultomten.nu"
-  New-ADGroup -GroupCategory:"Security" -GroupScope:"Global" -Name:"$Grp" -Path:"OU=Grupper,OU=Sundsvall,DC=jultomten,DC=nu" -SamAccountName:"$grp" -Server:"DC.jultomten.nu"
+  $sthlmGrp = "Stockholms$Grp"
+  $svallGrp = "Sundsvalls$Grp"
+  New-ADGroup -GroupCategory:"Security" -GroupScope:"Global" -Name:"$sthlmGrp" -Path:"OU=Grupper,OU=Stockholm,DC=jultomten,DC=nu" -SamAccountName:"$sthlmGrpgrp" -Server:"DC.jultomten.nu"
+  New-ADGroup -GroupCategory:"Security" -GroupScope:"Global" -Name:"$svallGrp" -Path:"OU=Grupper,OU=Sundsvall,DC=jultomten,DC=nu" -SamAccountName:"$svallGrpgrp" -Server:"DC.jultomten.nu"
 }
