f659cd6f871381ce3063ea611633f82e194aba55
LcLz0 <netharion@gmail.com>
2020-12-04 14:21:55 +0100

    update

--- Scenariolabb/grpCreate.ps1
+++
@@ -1,9 +0,0 @@
-$Groups = Import-Csv -Path "F:\grupper.csv"
-foreach ($Group in $Groups)
-{
-  $Grp = $Group.gName
-  $sthlmGrp = "Stockholms$Grp"
-  $svallGrp = "Sundsvalls$Grp"
-  New-ADGroup -GroupCategory:"Security" -GroupScope:"Global" -Name:"$sthlmGrp" -Path:"OU=Grupper,OU=Stockholm,DC=jultomten,DC=nu" -SamAccountName:"$sthlmGrpgrp" -Server:"DC.jultomten.nu"
-  New-ADGroup -GroupCategory:"Security" -GroupScope:"Global" -Name:"$svallGrp" -Path:"OU=Grupper,OU=Sundsvall,DC=jultomten,DC=nu" -SamAccountName:"$svallGrpgrp" -Server:"DC.jultomten.nu"
-}
