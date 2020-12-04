40322f3cd52ba20160a4821ff5b9ad9d4cb7f919
LcLz0 <netharion@gmail.com>
2020-12-04 12:09:08 +0100

    Localized all code, cleanup

--- Scenariolabb/bulkAddUser.ps1
+++ Scenariolabb/bulkAddUser.ps1
@@ -1,28 +1,26 @@
 ################## Initalise main importVars ##################
 
-$infoDump = Import-Csv -Path "F:\anvandarlistatest.csv"
+$infoDump = Import-Csv -Path "F:\userlist.csv"
 $groups = Import-Csv -Path "F:\groups.csv"
 $subUnits = Import-Csv -Path "F:\subUnits.csv"
 
 ################## Create main OU-structure ##################
 
-#  $stadad = $infoDump.stad | Select-Object -Unique
-
 # Get list of unique cities
 $uniqCities = $infoDump.stad | Select-Object -Unique
 
-# Create OU-structure
+### Create OU-structure
 
 # Create toplevel OU
 foreach ($City in $uniqCities)
 {
-  New-ADOrganizationalUnit -Name:"$City" -Path:"DC=jultomten,DC=nu"  -ProtectedFromAccidentalDeletion:$false
+  New-ADOrganizationalUnit -Name:"$City" -Path:"DC=cyberdyne,DC=io"  -ProtectedFromAccidentalDeletion:$false
   #echo $City
 
   # Create subOu in each toplevel
   foreach ($subOu in $subUnits)
   {
-    New-ADOrganizationalUnit -Name:$subOu.subOu -Path:"OU=$City,DC=jultomten,DC=nu" -ProtectedFromAccidentalDeletion:$false
+    New-ADOrganizationalUnit -Name:$subOu.subOu -Path:"OU=$City,DC=cyberdyne,DC=io" -ProtectedFromAccidentalDeletion:$false
     #echo $subOu.subOu
   }
 
@@ -30,31 +28,27 @@ foreach ($City in $uniqCities)
   foreach ($grp in $groups)
   {
     $grpName = $City + "s" + $grp.gName
-    New-ADGroup -GroupCategory:"Security" -GroupScope:"Global" -Name:$grpName -Path:"OU=Grupper,OU=$City,DC=jultomten,DC=nu" -SamAccountName:$grpName
+    New-ADGroup -GroupCategory:"Security" -GroupScope:"Global" -Name:$grpName -Path:"OU=Groups,OU=$City,DC=cyberdyne,DC=io" -SamAccountName:$grpName
     #echo $grpName
   }
 
 }
 
-<#
+################## Loop through user list and create all user accounts ##################
 
-
-
-
-
-foreach ($User in $Users)
+foreach ($User in $infoDump)
 {   # Main block
 
 	# Let's populate some variables, SAM creation further down
-    $FirstName = $User.fNamn
-    $LastName = $User.lNamn
+    $FirstName = $User.fName
+    $LastName = $User.sName
     $Displayname = $User.dName
-    $Description = $User.avdelning
-    $Mobile = $User.tele
-    $Address = $User.adress
-    $PostalCode = $User.postNr
-    $City = $User.stad
-    $OU = "OU=Användare,OU=$City,DC=jultomten,DC=nu"
+    $Description = $User.department
+    $Mobile = $User.phone
+    $Address = $User.address
+    $PostalCode = $User.zipCode
+    $City = $User.city
+    $OU = "OU=Users,OU=$City,DC=cyberdyne,DC=io"
 
 	# SAM name creation https://4sysops.com/archives/strings-in-powershell-replace-compare-concatenate-split-substring/
 	# $name.Remove(0.99) Remove everything except first char
@@ -64,8 +58,8 @@ foreach ($User in $Users)
     $UPN = "$SAM" + "@jultomten.nu"
 
 	# Run commands with above vars
-    New-ADUser -Name "$Displayname"-GivenName $FirstName -Surname $LastName -Description $Description -MobilePhone $Mobile -StreetAddress "$Address" -PostalCode "$PostalCode" -City $City -Path "$OU" -SamAccountName "$SAM" -UserPrincipalName "$UPN"
-    Unlock-ADAccount -Identity $SAM
+    New-ADUser -Name "$Displayname" -GivenName $FirstName -Surname $LastName -Description $Description -MobilePhone $Mobile -StreetAddress "$Address" -PostalCode "$PostalCode" -City $City -Path "$OU" -SamAccountName "$SAM" -UserPrincipalName "$UPN"
+    #Unlock-ADAccount -Identity $SAM
 }
 
 <#
